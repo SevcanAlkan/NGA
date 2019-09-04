5d5484c5bd105ae81e38c25b7f75afb6a7b061de
Sevcan ALKAN <sevcanalkan@outlook.com.tr>
2019-09-04 22:43:14 +0300

    It added to the project as basically. But it needs to make refactoring.

---
+++ NGA.API/Controllers/QueueController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NGA.Core;
+using NGA.Core.Helper;
+using NGA.Core.Model;
+using RabbitMQ.Client;
+
+namespace NGA.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class QueueController : ControllerBase
+    {
+        private ConnectionFactory factory;
+
+        public QueueController()
+        {
+            factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+            };
+        }
+
+        [HttpGet]
+        public JsonResult Setup()
+        {
+            try
+            {
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "NQueue",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+                }
+
+                return new JsonResult(APIResult.CreateVM(true));
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(APIResult.CreateVMWithError(ex, APIResult.CreateVM(false, null, AppStatusCode.ERR01001)));
+            }
+        }
+
+        public JsonResult SendNotification(NotificationVM model)
+        {
+            try
+            {
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    model.DateTime = DateTime.Now;
+
+                    var message = JsonConvert.SerializeObject(model);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "NQueue",
+                                         basicProperties: null,
+                                         body: body);
+
+
+                }
+                return new JsonResult(APIResult.CreateVM(true));
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(APIResult.CreateVMWithError(ex, APIResult.CreateVM(false, null, AppStatusCode.ERR01001)));
+            }
+        }
+    }
+}
---
+++ NGA.RabbitMQLogger/Program.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using NGA.Core.Model;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace NGA.RabbitMQLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+            };
+
+            bool continueToRuning = true;
+
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {                
+
+                channel.QueueDeclare(queue: "NQueue",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                do
+                {
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, mq) =>
+                    {
+                        var body = mq.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        NotificationVM notification = JsonConvert.DeserializeObject<NotificationVM>(message);
+                        Console.WriteLine($"[{notification.DateTime}] - Gonderen: {notification.SenderName}, Mesaj: {notification.Message} ");
+                    };
+
+                    channel.BasicConsume(queue: "NQueue",
+                                     autoAck: true,//true ise mesaj otomatik olarak kuyruktan silinir
+                                     consumer: consumer);
+                    
+                    
+
+                    System.Threading.Thread.Sleep(5000);
+
+                } while (continueToRuning);
+                             
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
